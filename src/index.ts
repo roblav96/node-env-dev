1a5cda14365b16e16ac3146b27dcca486abfb40e
Robert Laverty <roblav96@gmail.com>
2019-01-30 08:44:47 -0500

    v1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,46 @@
 
-import * as util from 'util'
-
-Object.assign(util.inspect, {
-	defaultOptions: {
-		breakLength: Infinity,
-		colors: true,
-		compact: false,
-		depth: 2,
-		maxArrayLength: Infinity,
-		showHidden: false,
-		showProxy: false,
-		sorted: true,
-	},
+process.on('uncaughtException', function(error) {
+	console.error(`████  GLOBAL UNCAUGHT EXCEPTION  ████`)
+	console.error(`error ->`, error)
 })
 
-Object.assign(util.inspect, {
-	styles: {
-		boolean: 'blue',
-		date: 'green',
-		null: 'red',
-		number: 'magenta',
-		regexp: 'green',
-		special: 'cyan',
-		string: 'green',
-		symbol: 'grey',
-		undefined: 'red',
-	},
+process.on('unhandledRejection', function(reason, promise) {
+	console.error(`████  GLOBAL UNHANDLED PROMISE REJECTION  ████`)
+	console.error(`reason ->`, reason)
+	console.error(`promise ->`, promise)
+	console.warn(`unhandledRejection invoking process.exit...`)
+	process.exit(1)
 })
 
-console.log(`\n████  ${new Date().toLocaleTimeString()}  ████\n`)
+try {
+	const util = require('util')
+	Object.assign(util.inspect, {
+		defaultOptions: {
+			breakLength: Infinity,
+			colors: true,
+			compact: false,
+			depth: 2,
+			maxArrayLength: Infinity,
+			showHidden: false,
+			showProxy: false,
+			sorted: true,
+		},
+	})
+	Object.assign(util.inspect, {
+		styles: {
+			boolean: 'blue',
+			date: 'green',
+			null: 'red',
+			number: 'magenta',
+			regexp: 'green',
+			special: 'cyan',
+			string: 'green',
+			symbol: 'grey',
+			undefined: 'red',
+		},
+	})
+	console.log(`node-env-dev assigned to util.inspect`)
+	// console.log(`████  ${new Date().toLocaleTimeString()}  ████\n`)
+} catch (error) {
+	console.error(`node-env-dev require catch Error ->`, error)
+}
