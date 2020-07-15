75eaa7e83f38c6e97ff39f07da886628e46449a4
Robert Laverty <roblav96@gmail.com>
2020-07-15 07:50:29 -0400

    export depth method

--- src/index.ts
+++ src/index.ts
@@ -42,22 +42,24 @@ try {
 } catch {}
 
 try {
-	global.process.DEVELOPMENT = global.process.env.NODE_ENV == 'development'
-	global.process.on('uncaughtException' as any, (error: Error, origin: string) => {
-		error.stack && Object.assign(error, { stack: cleanStack(error.stack) })
+	process.DEVELOPMENT = process.env.NODE_ENV == 'development'
+	process.on('uncaughtException', (error: Error, origin: string) => {
+		if (error.stack) Object.assign(error, { stack: cleanStack(error.stack) })
 		console.error(`${ansi.red.bold('[UNCAUGHT EXCEPTION]')}\n%O`, error)
 	})
-	global.process.on('unhandledRejection', (reason: Error, promise) => {
-		reason.stack && Object.assign(reason, { stack: cleanStack(reason.stack) })
+	process.on('unhandledRejection', (reason: Error, promise: Promise<any>) => {
+		if (reason.stack) Object.assign(reason, { stack: cleanStack(reason.stack) })
 		console.error(`${ansi.red.bold('[UNHANDLED REJECTION]')}\n%O`, reason)
 	})
 } catch {}
 
 try {
-	let banner = `${ansi.dim('■■■■■■■■■■■■■■■■■■■■■■■')}
-	      ${ansi.cyan.bold(dayjs().format('hh:mm:ss A'))}
-	${ansi.dim('■■■■■■■■■■■■■■■■■■■■■■■')}`
-	console.log(`\n${banner.replace(/\t/g, '').trim()}\n`)
+	if (process.env.NODE_ENV == 'development') {
+		let banner = `${ansi.dim('■■■■■■■■■■■■■■■■■■■■■■■')}
+		      ${ansi.cyan.bold(dayjs().format('hh:mm:ss A'))}
+		${ansi.dim('■■■■■■■■■■■■■■■■■■■■■■■')}`
+		console.log(`\n${banner.replace(/\t/g, '').trim()}\n`)
+	}
 
 	if (inspector.url()) {
 		let timeout = setInterval(Function, 1 << 30)
@@ -71,6 +73,10 @@ try {
 		console.clear()
 	}
 } catch {}
+
+export function depth(depth = Infinity) {
+	Object.assign(util.inspect.defaultOptions, { depth })
+}
 
 declare global {
 	namespace NodeJS {
